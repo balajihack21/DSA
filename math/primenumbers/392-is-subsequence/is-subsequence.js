f8437944d67ea02e1214a6c599331a8eb3917be8
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-01-25 11:21:42 +0530

    Time: 42 ms (97.25%) | Memory: 49.4 MB (5.10%) - LeetSync

--- math/primenumbers/392-is-subsequence/is-subsequence.js
+++ math/primenumbers/392-is-subsequence/is-subsequence.js
@@ -4,17 +4,15 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    let cnt=0
     let i=0
     let j=0
 
     while(i<s.length && j<t.length){
         if(s[i]==t[j]){
-            cnt++
             i++
         }
         j++
     }
-  return cnt==s.length
+  return i==s.length
 
 };
