d9d04c4d1574410cc8ec7767ed5ad199e3799d65
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-01-27 20:06:19 +0530

    Time: 66 ms (58.99%) | Memory: 50.8 MB (5.07%) - LeetSync

---
+++ math/primenumbers/1-two-sum/two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map={}
+
+    for(let i=0;i<nums.length;i++){
+        let a=target-nums[i]
+
+        if(map[a]!=undefined){
+            return [map[a],i]
+        }
+        else{
+            map[nums[i]]=i
+        }
+    }
+};
