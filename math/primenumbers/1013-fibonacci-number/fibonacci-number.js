537b24f37dea6f41177c3337f996cb51ee22620f
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-01-31 20:20:26 +0530

    Time: 39 ms (98.72%) | Memory: 48.8 MB (7.73%) - LeetSync

--- math/primenumbers/1013-fibonacci-number/fibonacci-number.js
+++ math/primenumbers/1013-fibonacci-number/fibonacci-number.js
@@ -3,11 +3,17 @@
  * @return {number}
  */
 var fib = function(n) {
-    if(n<=0){
-        return 0
+//    if(n<=1){
+//        return n
+//    }
+//     return fib(n-2)+fib(n-1)
+
+    let arr=new Array(n+1)
+    arr[0]=0
+    arr[1]=1
+    for(let i=2;i<=n;i++){
+         arr[i]=arr[i-1]+arr[i-2]
     }
-    if(n==1){
-        return 1
-    }
-    return fib(n-2)+fib(n-1)
+
+    return arr[n]
 };
