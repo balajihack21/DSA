5ebe1b7ea781ad0fc3a1c8c4a04d7d8a5617a6d9
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-01-31 15:25:34 +0530

    Time: 48 ms (94.74%) | Memory: 50.6 MB (5.85%) - LeetSync

---
+++ math/primenumbers/2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function(nums, target) {
+    nums.sort((a,b)=>{return a-b})
+let res=[]
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==target){
+           res.push(i)
+        }
+    }
+
+    return res
+};
