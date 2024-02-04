5eadff6655e4bbc48b95aba84ae41412309b7aea
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-02-04 13:52:21 +0530

    Time: 169 ms (40.18%) | Memory: 67.6 MB (7.67%) - LeetSync

---
+++ math/primenumbers/15-3sum/3sum.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    
+    let res=[]
+
+    nums.sort((a,b)=>{return a-b})
+    for(let k=0;k<nums.length;k++){
+        let i=k+1
+        let j=nums.length-1
+         if(nums[k]==nums[k-1]){
+             continue
+         }
+        while(i<j){
+            let sum=nums[k]+nums[i]+nums[j]
+            if(sum==0){
+                let trip=[]
+                trip.push(nums[k])
+                trip.push(nums[i])
+                trip.push(nums[j])
+                res.push(trip)
+                i++
+                j--
+            while(i<j &&  nums[i]==nums[i-1]){
+                i++
+            }
+            while(i<j && nums[j]==nums[j+1]){
+                j--
+            }
+            }
+            else if(sum<0){
+               i++
+            }
+            else{
+                j--
+            }
+        }
+        
+    }
+    return res
+};
