ab07525975587daa0ce88e0c1a73fbd569ed2430
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-03-20 10:44:42 +0530

    Time: 54 ms (68.82%) | Memory: 51.9 MB (59.58%) - LeetSync

--- math/primenumbers/792-binary-search/binary-search.js
+++ math/primenumbers/792-binary-search/binary-search.js
@@ -8,7 +8,7 @@ var search = function(nums, target) {
     let j=nums.length-1
 
     while(i<=j){
-       let mid=Math.floor((i+j)/2)
+       let mid=i+Math.floor((j-i)/2)
 
        if(nums[mid]==target){
             return mid
