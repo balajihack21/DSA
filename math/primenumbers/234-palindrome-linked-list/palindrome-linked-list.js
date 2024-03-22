5c239db6dbcf6031e20f02798b460a7b42075816
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-03-22 13:06:36 +0530

    Time: 166 ms (31.07%) | Memory: 79.9 MB (11.26%) - LeetSync

---
+++ math/primenumbers/234-palindrome-linked-list/palindrome-linked-list.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let fast=head
+   let arr=[]
+   let cnt=0
+     while(fast!=null){
+        arr.push(fast.val)
+        fast=fast.next
+        cnt++
+     }
+   console.log(arr)
+
+   let i=0
+   let j=arr.length-1
+   while(i<j){
+    if(arr[i]!=arr[j]){
+        return false
+    }
+    i++
+    j--
+   }
+
+   return true
+
+
+     
+};
