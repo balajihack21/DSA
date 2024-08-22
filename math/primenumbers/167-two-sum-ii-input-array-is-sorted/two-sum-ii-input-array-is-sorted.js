2a20b3984cadc5f51f188a6dae1a2773a85134bb
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-08-22 15:18:45 +0530

    Time: 48 ms (91.33%) | Memory: 50.2 MB (35.06%) - LeetSync

--- math/primenumbers/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ math/primenumbers/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,19 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-
-    let i=0
-    let j=numbers.length-1
-    while(i<j){
-        if(numbers[i]+numbers[j]==target){
-            return [i+1,j+1]
-        }
-        else if(numbers[i]+numbers[j]<target){
-            i++
-        }
-        else{
-            j--
-        }
+   let i=0
+   let j=numbers.length-1
+   while(i<j){
+    if(numbers[i]+numbers[j]==target){
+        return [i+1,j+1]
     }
+    else if(numbers[i]+numbers[j]<target){
+        i++
+    }
+    else{
+        j--
+    }
+   }
+   
 
 };
