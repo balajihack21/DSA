7205450c309891c01dfabe39c1be691e241739a4
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-01 20:28:32 +0530

    Time: 69 ms (54.22%) | Memory: 55.2 MB (56.43%) - LeetSync

--- math/primenumbers/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
+++ math/primenumbers/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -4,24 +4,53 @@
  */
 var reverseVowels = function(nums) {
 
-    let arr=['a','e','i','o','u','A','E','I','O','U']
+    // let arr=['a','e','i','o','u','A','E','I','O','U']
     let s=nums.split('')
+    // let i=0
+    // let j=s.length-1
+
+    // while(i<j){
+    //     while(i<j && arr.indexOf(s[i]) ==-1){
+    //         i++
+    //     }
+    //     while(i<j && arr.indexOf(s[j]) ==-1){
+    //         j--
+    //     }
+
+
+    //     [s[i],s[j]]=[s[j],s[i]]
+    //     i++
+    //     j--
+    // }
+
+    // return s.join('')
+
+    //aprroach -2
+
     let i=0
     let j=s.length-1
 
+function isvowel(ch){
+    let arr=['a','e','i','o','u','A','E','I','O','U']
+    if(arr.indexOf(ch)==-1){
+        return false
+    }
+  return true
+}
+
     while(i<j){
-        while(i<j && arr.indexOf(s[i]) ==-1){
+        if(!isvowel(s[i])){
             i++
         }
-        while(i<j && arr.indexOf(s[j]) ==-1){
+        else if(!isvowel(s[j])){
             j--
         }
-
-
-        [s[i],s[j]]=[s[j],s[i]]
-        i++
-        j--
+        else{
+            [s[i],s[j]]=[s[j],s[i]]
+            i++
+            j--
+        }
     }
+  return s.join('')
 
-    return s.join('')
 };
