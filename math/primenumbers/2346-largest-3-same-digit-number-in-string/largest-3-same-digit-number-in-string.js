328d0a61ade70226eb2e24c0b5aab3c2015f48e6
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-03 19:15:05 +0530

    Time: 67 ms (12.07%) | Memory: 52.3 MB (13.79%) - LeetSync

--- math/primenumbers/2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.js
+++ math/primenumbers/2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.js
@@ -4,9 +4,9 @@
  */
 var largestGoodInteger = function(num) {
     let str=""
-    for(let i=0;i<num.length-2;i++){
-        if(num[i]==num[i+1] && num[i]==num[i+2]){
-            str1=num[i]+num[i+2]+num[i+1]
+    for(let i=1;i<num.length;i++){
+        if(num[i]==num[i+1] && num[i]==num[i-1]){
+            str1=num[i]+num[i-1]+num[i+1]
             if(str1>str){
                 str=str1
             }
