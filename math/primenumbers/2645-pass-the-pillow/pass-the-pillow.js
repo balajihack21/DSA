d7762d54a171a2afd9d233b4960196483fe6915b
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-03 13:46:54 +0530

    Time: 51 ms (61.69%) | Memory: 48.5 MB (80.31%) - LeetSync

---
+++ math/primenumbers/2645-pass-the-pillow/pass-the-pillow.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @param {number} time
+ * @return {number}
+ */
+var passThePillow = function(n, time) {
+   let ind=1
+   let dir=1
+
+   while(time>0){
+    if(ind==1){
+        dir=1
+    }else if(ind==n){
+        dir=-1
+    }
+    ind+=dir
+    time--
+   }
+
+   return ind
+    
+};
