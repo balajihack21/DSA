ea2267e3bcc3e332cdbf61aba4280d403769fd27
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-18 17:11:05 +0530

    Time: 52 ms (60.33%) | Memory: 49 MB (34.98%) - LeetSync

--- math/primenumbers/2507-number-of-common-factors/number-of-common-factors.js
+++ math/primenumbers/2507-number-of-common-factors/number-of-common-factors.js
@@ -5,13 +5,8 @@
  */
 var commonFactors = function(a, b) {
     let cnt=1
-    let n=0
-    if(a>b){
-        n=a
-    }
-    else{
-      n=b
-    }
+    let n=(a>b)?a:b
+    
 
     for(let i=2;i<=n;i++){
         if(a%i==0 && b%i==0){
