cd97f2d970b67c2a2d9918a25dc89080f82e1437
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-19 13:14:39 +0530

    Time: 121 ms (50.00%) | Memory: 50.2 MB (93.48%) - LeetSync

--- math/primenumbers/149-max-points-on-a-line/max-points-on-a-line.js
+++ math/primenumbers/149-max-points-on-a-line/max-points-on-a-line.js
@@ -3,32 +3,31 @@
  * @return {number}
  */
 var maxPoints = function(points) {
-        const n = points.length;
-    if (n === 1) return 1;
+    
+    let n=points.length
+   if(n==1){
+    return 1
+   }
+   let res=0
+    for(let i=0;i<n;i++){
+        for(let j=i+1;j<n;j++){
+            let cnt=2
+            let dy=points[j][1]-points[i][1]
+            let dx=points[j][0]-points[i][0]
 
-    let result = 0;
+            for(let k=0;k<n;k++){
+                if(k!=i && k!=j){
+                   let  d_y=points[k][1]-points[i][1]
+                    let d_x=points[k][0]-points[i][0]
 
-    for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            let count = 2;
-
-            const dx = points[j][0] - points[i][0];
-            const dy = points[j][1] - points[i][1];
-
-            for (let k = 0; k < n; k++) {
-                if (k !== i && k !== j) {
-                    const dx_ = points[k][0] - points[i][0];
-                    const dy_ = points[k][1] - points[i][1];
-
-                    if (dx_ * dy === dy_ * dx) {
-                        count++;
+                    if(dy*d_x==d_y*dx){
+                        cnt++
                     }
                 }
             }
-
-            result = Math.max(result, count);
+            res=Math.max(res,cnt)
         }
     }
-
-    return result;
+    console.log(res)
+    return res
 };
