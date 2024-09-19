986d62a13051d9819780967dca9624402fc3e12e
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-19 12:07:26 +0530

    Time: 61 ms (25.24%) | Memory: 50.7 MB (48.57%) - LeetSync

--- math/primenumbers/1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.js
+++ math/primenumbers/1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.js
@@ -3,20 +3,18 @@
  * @return {boolean}
  */
 var checkStraightLine = function(coordinates) {
-    let n=coordinates.length
+    
     let d_y=coordinates[1][1]-coordinates[0][1]
     let d_x=coordinates[1][0]-coordinates[0][0]
 
-    
+    for(let i=2;i<coordinates.length;i++){
+        let d1_y=coordinates[i][1]-coordinates[0][1]
+        let d1_x=coordinates[i][0]-coordinates[0][0]
 
-for(let i=2;i<n;i++){
-    let d1_y=coordinates[i][1]-coordinates[0][1]
-    let d1_x=coordinates[i][0]-coordinates[0][0]
-    if(d_y*d1_x!=d1_y*d_x){
-        return false
+        if(d_y*d1_x!=d1_y*d_x){
+            return false
+        }
     }
-}
-    
 
     return true
 };
