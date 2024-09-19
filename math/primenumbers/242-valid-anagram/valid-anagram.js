a4e37d364248bfb3b6505762fb6e8bc6a418846f
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-19 17:10:42 +0530

    Time: 64 ms (80.16%) | Memory: 51.8 MB (66.85%) - LeetSync

--- math/primenumbers/242-valid-anagram/valid-anagram.js
+++ math/primenumbers/242-valid-anagram/valid-anagram.js
@@ -4,7 +4,28 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let str=s.split('').sort().join("")
-    let str1=t.split('').sort().join("")
-    return str==str1
+    // let str=s.split('').sort().join("")
+    // let str1=t.split('').sort().join("")
+    // return str==str1
+
+
+    let arr=new Array(26).fill(0)
+
+    for(let i=0;i<s.length;i++){
+        arr[s.charCodeAt(i)-97]++
+    }
+    
+    for(let i=0;i<t.length;i++){
+        arr[t.charCodeAt(i)-97]--
+    }
+
+    console.log(arr)
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]!=0){
+            return false
+        }
+    }
+
+    return true
 };
