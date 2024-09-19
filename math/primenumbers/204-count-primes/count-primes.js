4b9f710849b2e9603ece1e8a5385a768d6a78952
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-19 07:41:22 +0530

    Time: 395 ms (37.32%) | Memory: 149.8 MB (13.83%) - LeetSync

--- math/primenumbers/204-count-primes/count-primes.js
+++ math/primenumbers/204-count-primes/count-primes.js
@@ -5,25 +5,26 @@
 var countPrimes = function(n) {
     let arr=new Array(n).fill(true)
 
-    arr[1]=false
+arr[1]=false
 
-   for(let i=2;i*i<n;i++){
-    if(arr[i]==false){
-        continue;
-    }
 
-    for(let j=i*i;j<n;j=j+i){
-        arr[j]=false
-    }
-   }
-
-   console.log(arr)
+for(let i=2;i*i<=n;i++){
+  if(arr[i]==false){
+    continue
+  }
+  
+  for(let j=i*i;j<=n;j=j+i){
+    arr[j]=false
+  }
+}
 
 let cnt=0
-   for(let i=2;i<n;i++){
-     if(arr[i]){
-        cnt++
-     }
-   }
-   return cnt
+
+for(let i=2;i<n;i++){
+  if(arr[i]){
+    cnt++
+  }
+}
+return cnt
+
 };
