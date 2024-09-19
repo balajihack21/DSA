66157bda28eda07a9289e2f0b0b8e8edb7b11d79
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-19 18:59:50 +0530

    Time: 61 ms (90.25%) | Memory: 53.4 MB (27.96%) - LeetSync

---
+++ math/primenumbers/383-ransom-note/ransom-note.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+   let arr=new Array(26).fill(0)
+
+   for(let i=0;i<ransomNote.length;i++){
+     arr[ransomNote.charCodeAt(i)-97]++
+   }
+
+    for(let i=0;i<magazine.length;i++){
+     if(arr[magazine.charCodeAt(i)-97]!=0){
+        arr[magazine.charCodeAt(i)-97]--
+   }
+
+    }
+   console.log(arr)
+   for(let i=0;i<arr.length;i++){
+    if(arr[i]!=0){
+        return false
+    }
+   }
+   return true
+};
