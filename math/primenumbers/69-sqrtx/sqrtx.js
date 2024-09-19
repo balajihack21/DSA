bd1c39706893d3aeb68c4a6afebfd75886a89531
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-19 14:03:26 +0530

    Time: 119 ms (6.26%) | Memory: 53.5 MB (6.71%) - LeetSync

---
+++ math/primenumbers/69-sqrtx/sqrtx.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if(x==0){
+        return 0
+    }
+    if(x<4){
+        return 1
+    }
+   
+    let cnt=0
+    for(let i=2;i*i<=x;i++){
+       cnt=i
+    }
+
+    console.log(Math.floor(cnt))
+    return cnt
+};
