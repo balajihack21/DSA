e69417cd9d9fe1aad0aaa8fcbfdd5d53156449e4
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-20 22:13:57 +0530

    Time: 66 ms (24.06%) | Memory: 48.9 MB (97.29%) - LeetSync

--- math/primenumbers/389-find-the-difference/find-the-difference.js
+++ math/primenumbers/389-find-the-difference/find-the-difference.js
@@ -6,17 +6,22 @@
 var findTheDifference = function(s, t) {
     let arr=new Array(26).fill(0)
 
-    for(let i=0;i<t.length;i++){
-        arr[t.charCodeAt(i)-97]++
+    for(let i=0;i<s.length;i++){
+        arr[s.charCodeAt(i)-97]++
     }
 
-    for(let j=0;j<s.length;j++){
-        arr[s.charCodeAt(j)-97]--
-    }
-
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]==1){
-            return String.fromCharCode(i+97)
+    for(let j=0;j<t.length;j++){
+        if(arr[t.charCodeAt(j)-97]){
+            arr[t.charCodeAt(j)-97]--
+        }
+        else{
+            return t[j]
         }
     }
+
+    // for(let i=0;i<arr.length;i++){
+    //     if(arr[i]==1){
+    //         return String.fromCharCode(i+97)
+    //     }
+    // }
 };
