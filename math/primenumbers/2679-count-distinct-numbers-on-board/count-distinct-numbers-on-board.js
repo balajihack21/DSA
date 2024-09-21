cb22b45628ba1dd29266b44e65eb37a69374d791
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-09-21 12:36:59 +0530

    Time: 55 ms (42.48%) | Memory: 47.8 MB (100.00%) - LeetSync

--- math/primenumbers/2679-count-distinct-numbers-on-board/count-distinct-numbers-on-board.js
+++ math/primenumbers/2679-count-distinct-numbers-on-board/count-distinct-numbers-on-board.js
@@ -3,22 +3,24 @@
  * @return {number}
  */
 var distinctIntegers = function(n) {
-    let arr=[n]
-     const res = new Set();
-    res.add(n);
-    let cnt=0
-    while(arr.length>0){
-        let a=arr.shift()
+    // let arr=[n]
+    //  const res = new Set();
+    // res.add(n);
+    // let cnt=0
+    // while(arr.length>0){
+    //     let a=arr.shift()
 
-        for(let i=1;i<=a;i++){
-            if(a%i==1){
-                if(!res.has(i)){
-                    res.add(i)
-                    arr.push(i)
-                }
-            }
-        }
-    }
-       console.log(res)
-       return res.size
+    //     for(let i=1;i<=a;i++){
+    //         if(a%i==1){
+    //             if(!res.has(i)){
+    //                 res.add(i)
+    //                 arr.push(i)
+    //             }
+    //         }
+    //     }
+    // }
+    //    console.log(res)
+    //    return res.size
+
+    return n==1?1:n-1
 };
