c05225a5061663eae17148bf35c64988f4bf66fe
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-10-06 15:39:47 +0530

    Time: 359 ms (6.00%) | Memory: 83.6 MB (5.90%) - LeetSync

--- math/primenumbers/219-contains-duplicate-ii/contains-duplicate-ii.js
+++ math/primenumbers/219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -8,10 +8,8 @@ var containsNearbyDuplicate = function(nums, k) {
 
     for(let i=0;i<nums.length;i++){
         if(map[nums[i]]!=undefined){
-            console.log(map[nums[i]])
             let ab=Math.abs(map[nums[i]]-i)
             map[nums[i]]=i
-            console.log(ab)
             if(ab<=k){
                 return true
             }
