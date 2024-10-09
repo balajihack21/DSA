130fa0d082e65ccfd63faf342714544ca96799b5
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-10-09 13:11:28 +0530

    Time: 55 ms (64.63%) | Memory: 49.5 MB (72.56%) - LeetSync

--- math/primenumbers/1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.js
+++ math/primenumbers/1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.js
@@ -4,23 +4,22 @@
  */
 var halvesAreAlike = function(s) {
     let vowels=['a','e','i','o','u','A','E','I','O','U']
-   
-  let n=s.length
-  let i=0
-  let cnt=0
-  let cnt1=0
-  for(;i<Math.floor(n/2);i++){
-    if(vowels.indexOf(s[i])!=-1){
-        cnt++
+
+    let cnt=0
+    let cnt1=0
+    let n=s.length
+    let i=0
+    let j=Math.floor(n/2)
+    while(i<j && j<n){
+        if(vowels.indexOf(s[i])!=-1){
+            cnt++
+        }
+        if(vowels.indexOf(s[j])!=-1){
+            cnt1++
+        }
+        i++
+        j++
     }
-  }
 
-  for(let j=i;j<n;j++){
-    if(vowels.indexOf(s[j])!=-1){
-        cnt1++
-    }
-  }
-
-   return cnt==cnt1
-
+    return cnt==cnt1
 };
