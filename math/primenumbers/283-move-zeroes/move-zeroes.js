8ff5a1588e0aaca34e97882911af5d7b6031f01b
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-11-10 19:00:44 +0530

    Time: 5 ms (34.18%) | Memory: 56.2 MB (5.17%) - LeetSync

--- math/primenumbers/283-move-zeroes/move-zeroes.js
+++ math/primenumbers/283-move-zeroes/move-zeroes.js
@@ -3,20 +3,25 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    
+    if(nums.length==1){
+        return nums
+    }
+    //[1,3,0,0,12]
     let i=0
     let j=1
 
     while(j<nums.length){
         if(nums[i]!=0){
-            i++
+           i++
             j++
         }
         else if(nums[j]==0){
-           j++
+            j++
         }
         else{
             [nums[i],nums[j]]=[nums[j],nums[i]]
+            
         }
     }
+return nums
 };
