3a0964d78c510d784571f0ee89505cab52e2ce6f
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2024-11-16 14:02:45 +0530

    Time: 4 ms (97.98%) | Memory: 51.8 MB (90.16%) - LeetSync

--- math/primenumbers/383-ransom-note/ransom-note.js
+++ math/primenumbers/383-ransom-note/ransom-note.js
@@ -3,23 +3,24 @@
  * @param {string} magazine
  * @return {boolean}
  */
-var canConstruct = function(ransomeNote, magazine) {
+var canConstruct = function(ransomNote, magazine) {
     let arr=new Array(26).fill(0)
 
-    for(let i=0;i<ransomeNote.length;i++){
-        arr[ransomeNote.charCodeAt(i)-97]++
+    for(let i=0;i<ransomNote.length;i++){
+        arr[ransomNote.charCodeAt(i)-97]++
     }
 
-    for(let i=0;i<magazine.length;i++){
-        if(arr[magazine.charCodeAt(i)-97]!=0){
-            arr[magazine.charCodeAt(i)-97]--
-        }
+  for(let i=0;i<magazine.length;i++){
+       if(arr[magazine.charCodeAt(i)-97]!=0){
+         arr[magazine.charCodeAt(i)-97]--
+       }
     }
 
-      for(let i=0;i<arr.length;i++){
+    for(let i=0;i<arr.length;i++){
         if(arr[i]!=0){
             return false
         }
-      }
-      return true
+    }
+
+    return true
 };
