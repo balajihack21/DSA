7258d266dba2746791564f8c5a4761833bcb893c
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2025-01-28 18:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (25.16%) - LeetSync

---
+++ math/primenumbers/35-search-insert-position/search-insert-position.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let s=0
+    let e=nums.length-1
+
+    while(s<=e){
+
+        let mid=Math.floor((s+e)/2)
+
+        if(nums[mid]==target){
+            return mid
+        }
+        else if(nums[mid]>target){
+            e=mid-1
+        }
+        else{
+            s=mid+1
+        }
+    }
+
+    return e+1
+};
