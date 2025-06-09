f622eb43aa34f21bf21e17e7266b6408833bf4ca
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2025-06-09 18:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (90.65%) - LeetSync

--- math/primenumbers/35-search-insert-position/search-insert-position.js
+++ math/primenumbers/35-search-insert-position/search-insert-position.js
@@ -4,23 +4,21 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let s=0
-    let e=nums.length-1
+   let l=0
+   let r=nums.length-1
 
-    while(s<=e){
+   while(l<=r){
+    let mid=Math.floor((l+r)/2)
 
-        let mid=Math.floor((s+e)/2)
-
-        if(nums[mid]==target){
-            return mid
-        }
-        else if(nums[mid]>target){
-            e=mid-1
-        }
-        else{
-            s=mid+1
-        }
+    if(nums[mid]==target){
+        return mid
     }
-
-    return e+1
+    else if(nums[mid]<target){
+        l=mid+1
+    }
+    else{
+        r=mid-1
+    }
+   }
+   return r+1
 };
