f69cd77c97350efc490ba81184ea20ab9c972de9
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2025-07-06 14:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 57.7 MB (5.07%) - LeetSync

--- math/primenumbers/78-subsets/subsets.js
+++ math/primenumbers/78-subsets/subsets.js
@@ -2,16 +2,16 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsets = function(nums,res=[]) {
-    if(nums.length==0){
+var subsets = function(nums,res=[],i=0) {
+    if(nums.length==i){
         return  [res]
     }
   
 
-  let c=nums[0]
-  nums.shift()
-  let w=subsets([...nums],[...res,c])
-  let wi=subsets([...nums],[...res])
+  let c=nums[i]
+//   nums.shift()
+  let w=subsets(nums,[...res,c],i+1)
+  let wi=subsets(nums,res,i+1)
 
   return w.concat(wi)
 
