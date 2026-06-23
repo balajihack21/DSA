73f9b53e26044378f5ad69e485543a5ceccfbc51
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2026-06-23 20:28:23 +0530

    Time: 0 ms (100.00%) | Memory: 64.7 MB (40.77%) - LeetSync

--- math/primenumbers/882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.js
+++ math/primenumbers/882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.js
@@ -3,15 +3,23 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(nums) {
-    let index=0
-    let max=nums[0]
+        let l=0
+    let r=nums.length-1
 
-    for(let i=1;i<nums.length;i++){
-        if(nums[i]>max){
-            max=nums[i]
-            index=i
+    while(l<=r){
+        let mid=Math.floor((l+r)/2)
+
+        if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+            return mid
+        }
+        else if(nums[mid]<nums[mid+1]){
+            l=mid+1
+        }
+        else{
+            r=mid-1
         }
     }
 
-    return index
+    return l
+    
 };
