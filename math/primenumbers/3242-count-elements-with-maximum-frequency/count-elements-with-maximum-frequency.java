6afc6d48623c31bc86bc1ae72cd6bab00236f5e7
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2026-06-27 13:26:26 +0530

    Time: 1 ms (99.69%) | Memory: 43.9 MB (81.66%) - LeetSync

---
+++ math/primenumbers/3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+            if(nums.length==1){
+                return 1;
+            }
+
+        int n = nums.length;
+        int[] arr = new int[128];
+        int max = Integer.MIN_VALUE;
+        int sum=0;
+
+        for (int i = 0; i < n; i++) {
+            arr[nums[i]]++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==max){
+                sum+=arr[i];
+            }
+        }
+
+
+        return sum;
+    }
+}
