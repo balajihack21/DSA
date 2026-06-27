2388a242703b3bfb01b88bb6dec88dc3daa7efd8
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2026-06-27 14:15:45 +0530

    Time: 58 ms (5.29%) | Memory: 115.2 MB (6.09%) - LeetSync

---
+++ math/primenumbers/217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        int n=nums.length;
+        HashSet<Integer> set=new HashSet<>();
+
+        for(int i=0;i<n;i++){
+            
+            if(set.contains(nums[i])){
+                return true;
+            }
+            else{
+                set.add(nums[i]);
+            }
+
+        }
+        System.out.println(set);
+
+        
+        return false;
+    }
+}
