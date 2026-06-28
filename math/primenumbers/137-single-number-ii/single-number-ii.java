d460ccc2a6875d239e396f09995cd7dedb1c10b1
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2026-06-28 14:23:06 +0530

    Time: 6 ms (41.82%) | Memory: 46.5 MB (20.78%) - LeetSync

---
+++ math/primenumbers/137-single-number-ii/single-number-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+
+        for(Map.Entry<Integer,Integer> pair:map.entrySet()){
+                if(pair.getValue()==1){
+                    return pair.getKey();
+                }
+        }
+
+        return -1;
+        
+    }
+}
