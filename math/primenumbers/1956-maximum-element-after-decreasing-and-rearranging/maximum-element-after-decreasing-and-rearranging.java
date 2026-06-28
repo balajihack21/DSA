bfb6459b42da4fe7700c32f0a5e04561926a443d
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2026-06-28 15:06:54 +0530

    Time: 11 ms (12.08%) | Memory: 77.2 MB (92.08%) - LeetSync

---
+++ math/primenumbers/1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        
+        Arrays.sort(arr);
+
+        
+        int previous=1;
+        int current=0;
+
+        for(int i=1;i<arr.length;i++){
+            current=arr[i];
+            if(current==previous+1){
+                previous=current;
+            }
+            else if(current<previous+1){
+                previous=current;
+
+            }
+            else{
+                 previous=previous+1;
+            }
+        }
+
+        System.out.println(previous);
+        return previous;
+    }
+}
