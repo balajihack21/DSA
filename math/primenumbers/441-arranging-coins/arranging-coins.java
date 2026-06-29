315879171a89f47d199ad7cabacb4422c8537977
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2026-06-29 15:23:59 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (61.41%) - LeetSync

--- math/primenumbers/441-arranging-coins/arranging-coins.java
+++ math/primenumbers/441-arranging-coins/arranging-coins.java
@@ -1,15 +1,24 @@
 class Solution {
     public int arrangeCoins(int n) {
+        int ans=0;
 
-        int cnt=0;
+        int left=1;
+        int right=n;
 
-        for(int i=1;i<=n;i++){
-            if(n>=i){
-                cnt++;
-                n=n-i;
+        while(left<=right){
+            int mid = left + (right - left) / 2;
+
+            if((long)mid*(mid+1)/2<=n){
+                ans=mid;
+                left=mid+1;
+                
+            }
+            else{
+                right=mid-1;
             }
         }
+
+        return ans;
         
-        return cnt;
     }
 }
