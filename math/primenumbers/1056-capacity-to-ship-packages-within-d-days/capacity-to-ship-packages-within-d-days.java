b055d9eef6243e7f388892d5d34a65e1df98b4bb
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2026-06-29 18:56:50 +0530

    Time: 10 ms (84.83%) | Memory: 50.1 MB (74.93%) - LeetSync

--- math/primenumbers/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ math/primenumbers/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i=0;i<weights.length;i++){
             sum+=weights[i];
             
-            
             if(sum>n){
                 res++;
                 sum=weights[i];
