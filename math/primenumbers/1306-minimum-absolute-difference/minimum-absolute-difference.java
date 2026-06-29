4e0e86ef3b826b3d966df49895ef820c6c904e71
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2026-06-29 21:31:45 +0530

    Time: 26 ms (5.05%) | Memory: 64.7 MB (5.63%) - LeetSync

---
+++ math/primenumbers/1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+
+        Arrays.sort(arr);
+
+        List<List<Integer>> ans=new ArrayList<>();
+        int min=Integer.MAX_VALUE;
+
+        for(int i=1;i<arr.length;i++){
+            int temp=Math.abs(arr[i]-arr[i-1]);
+            if(temp<min){
+                min=temp;
+            }
+        }
+
+        for(int i=1;i<arr.length;i++){
+            List<Integer> res=new ArrayList<>();
+            int temp=Math.abs(arr[i]-arr[i-1]);
+            if(temp==min){
+                res.add(arr[i-1]);
+                res.add(arr[i]);
+                ans.add(res);
+            }
+            
+            
+        }
+        System.out.println(ans);
+
+        return ans;
+    }
+}
