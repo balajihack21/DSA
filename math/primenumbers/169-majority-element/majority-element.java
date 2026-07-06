376d854b1aab01cda9de8fc2d3a19eab255902d8
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2026-07-06 10:41:57 +0530

    Time: 1 ms (99.89%) | Memory: 55.6 MB (59.55%) - LeetSync

---
+++ math/primenumbers/169-majority-element/majority-element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int vote=0;
+        int candidate =-1;
+        int cnt=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(vote==0){
+                candidate =nums[i];
+            }
+            if(candidate==nums[i]){
+                vote++;
+            }
+            else{
+                vote--;
+            }
+        }
+
+        
+return candidate;
+
+        
+    }
+}
