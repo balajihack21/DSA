d0e64d4070711ace73106a54b682479ba0c8bb1a
balaji.tech <72429891+balajihack21@users.noreply.github.com>
2026-07-16 09:36:08 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (69.39%) - LeetSync

--- math/primenumbers/792-binary-search/binary-search.java
+++ math/primenumbers/792-binary-search/binary-search.java
@@ -1,22 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left=0;
-        int right=nums.length-1;
 
-        while(left<=right){
-            int mid=left+(right-left)/2;
+        int low=0;
+        int high=nums.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
 
             if(nums[mid]==target){
                 return mid;
             }
             else if(nums[mid]>target){
-                right=mid-1;
+                high=mid-1;
             }
             else{
-                left=mid+1;
+                low=mid+1;
             }
+
+            
         }
-        
+
         return -1;
+        
     }
 }
